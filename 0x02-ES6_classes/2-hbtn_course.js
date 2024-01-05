0444c8cb717cc98edfeb99fea70e916f69924dc4
tituz175 <tobititus01@gmail.com>
2024-01-04 20:43:58 -0700

    task 2 done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a string');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (typeof value !== 'object') {
+      throw new TypeError('Students must be a string');
+    }
+    this._students = value;
+  }
+}
