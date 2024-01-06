8ad3e64f7fbd223ecb0183fb06c856f803a3b996
tituz175 <tobititus01@gmail.com>
2024-01-05 19:59:53 -0700

    task 1 done

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
 export default function getFullResponseFromAPI(succcess) {
   return new Promise((resolve, reject) => {
     if (succcess) {
-      return resolve({ status: 200, body: "Success" });
+      return resolve({ status: 200, body: 'Success' });
     }
-    return reject(new Error("The fake API is not working currently"));
-  })
+    return reject(new Error('The fake API is not working currently'));
+  });
 }
