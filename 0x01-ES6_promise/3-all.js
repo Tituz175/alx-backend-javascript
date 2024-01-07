b2ea5e10705d6fff13680ede61355353d279e6ea
tituz175 <tobititus01@gmail.com>
2024-01-06 21:59:39 -0700

    task 3 done

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto().then((res1) => createUser().then((res2) => {
+    console.log(`${res1.body} ${res2.firstName} ${res2.lastName}`);
+  }));
+}
