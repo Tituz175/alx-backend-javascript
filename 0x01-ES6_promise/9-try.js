63d7a6beb42451f1245841daa319bb612a0ed011
tituz175 <tobititus01@gmail.com>
2024-01-06 23:38:15 -0700

    task 9 done

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = ['Guardrail was processed'];
+
+  try {
+    queue.unshift(mathFunction());
+  } catch (error) {
+    queue.unshift(`Error: ${error.message}`);
+  }
+
+  return queue;
+}
